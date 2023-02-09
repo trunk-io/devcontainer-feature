f94c30dd08a214e2783826b596e8ee724ab29134
Lauri Takacsi <ltakacsi@gmail.com>
2023-02-09 01:02:17 +0000

    install curl in install script

--- test/trunk/test.sh
+++ test/trunk/test.sh
@@ -40,7 +40,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "execute command" bash -c "trunk help | grep trunk [flags] [subcommand]"
+check "execute command" bash -c "trunk --help | grep 'trunk \[flags\] \[subcommand\]'"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
